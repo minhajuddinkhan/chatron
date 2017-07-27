fb07a92c464a03d0a201831078c814521a5a3448
Minhaj <minhajuddin.khan@tenpearls.com>
2017-07-27 16:42:02 +0500

    side-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  chatList = [
+    {
+      name: "Ali",
+      "message": "han bhai kese ho?",
+      image : "http://i2.wp.com/you-log.com/wp-content/uploads/2015/08/People-Avatar-Set-Circular-09.png?resize=250%2C250"
+    },
+    {
+      name: "Ali",
+      "message": "han bhai kese ho?",
+      image : "http://i2.wp.com/you-log.com/wp-content/uploads/2015/08/People-Avatar-Set-Circular-09.png?resize=250%2C250"
+    },
+    {
+      name: "Ali",
+      "message": "han bhai kese ho?",
+
+      image : "http://i2.wp.com/you-log.com/wp-content/uploads/2015/08/People-Avatar-Set-Circular-09.png?resize=250%2C250"
+    },
+    {
+      name: "Ali",
+      "message": "han bhai kese ho?",
+
+      image : "http://i2.wp.com/you-log.com/wp-content/uploads/2015/08/People-Avatar-Set-Circular-09.png?resize=250%2C250"
+    },
+    {
+      name: "Ali",
+      "message": "han bhai kese ho?",
+      image : "http://i2.wp.com/you-log.com/wp-content/uploads/2015/08/People-Avatar-Set-Circular-09.png?resize=250%2C250"
+    }
+
+  ]
 }
